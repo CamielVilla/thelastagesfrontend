992708f493f77b41c2006fbe23bd49ea3b48ca79
CamielVilla <camielvilla@gmail.com>
2023-01-18 13:36:53 +0100

    added post option

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import information from "../src/Assets/Spelinformatie.png"
+import axios from "axios";
+import { useForm} from "react-hook-form";
 
 function App() {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const [addSucces, toggleAddSucces] = useState(false);
+    async function handleFormSubmit(data){
+        console.log(data.email);
+        try{
+            const response = await axios.post('http://localhost:8080/thelastages/addemail',{
+                emailAddress: data.email,
+                })
+            console.log(data.email)
+            toggleAddSucces(true)
+            const emailField = document.getElementById("email");
+            emailField.value = "";
+        }catch (data) {
+            console.error(data)
+        }
+    }
   return (
       <>
       <div className="homepage">
@@ -13,14 +31,26 @@ function App() {
       </header>
           </div>
           <div className='form-container'>
-          <form onSubmit={console.log("hij doet het")}>
-              <h3>Leave your e-mail for more information</h3>
+          <form onSubmit={handleSubmit(handleFormSubmit)} >
+              <h3>Leave your e-mail to receive more information</h3>
               <input
-                  name="email-adres"
+                  id="email"
                   type="email"
                   placeholder="your e-mail address"
+                  {...register("email",
+                      {required: {
+                          value: true,
+                              message: 'enter an e-mail address',
+                          },
+                          maxLength: {
+                          value: 50,
+                              message: "max length is 50 characters",
+                          }
+                      })}
                 />
+              {errors.email && <p>{errors.email.message}</p>}
               <button type="submit">send</button>
+              {addSucces && <p>Thank you for your interest in The Last Ages</p>}
           </form>
           </div>
           <div className="bottom-container">
