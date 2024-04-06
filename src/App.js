595e579f5b50f71a9831abcb68de86de19f3f7eb
CamielVilla <camielvilla@gmail.com>
2024-04-06 11:39:37 +0200

    add routing and privacy policy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import React from "react";
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
 import MainSite from "./Pages/MainSite";
 import Privacy from "./Pages/Privacy";
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainSite />,
-    },
-    {
-        path: '/privacy',
-        element: <Privacy />,
-    },
-]);
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+            <Route index element={<MainSite />}/>
+            <Route path="privacy" element={<Privacy />}/>
+        </Route>
+    )
+)
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <RouterProvider router={router} />
+    )
+
 }
 export default App;
